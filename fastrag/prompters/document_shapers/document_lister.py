f7d307eba9cc9f064d246785e491b876147dbff0
Moshe Berchansky <moshe.berchansky@intel.com>
2023-04-02 12:20:54 +0300

    added gpt rerank plus read (#11)

---
+++ fastrag/prompters/document_shapers/document_lister.py
@@ -0,0 +1,47 @@
+from haystack import BaseComponent
+from typing import Any, Tuple, Optional, Dict, List
+from haystack.schema import Document
+
+class DocumentLister(BaseComponent):
+
+    outgoing_edges = 1
+
+    def __init__(
+        self,
+    ):
+        super().__init__()
+        
+    def run(  # type: ignore
+        self,
+        query: Optional[str] = None,
+        documents: Optional[List[Document]] = None,
+        invocation_context: Optional[Dict[str, Any]] = None,
+    ) -> Tuple[Dict, str]:
+        
+        context_text = ""
+        for doc_index, doc in enumerate(documents):
+            context_text += f"Paragraph {doc_index+1}: {doc.content}\n\n"
+            
+        joined_doc = Document(content=context_text)
+        
+        results = {
+            "documents": [joined_doc],
+            "invocation_context": {
+                "query": query,
+                "documents": [joined_doc],
+            }
+        }
+            
+        return results, "output_1"
+
+    def run_batch(  # type: ignore
+        self,
+        query: Optional[str] = None,
+        documents: Optional[List[Document]] = None,
+        invocation_context: Optional[Dict[str, Any]] = None,
+    ) -> Tuple[Dict, str]:
+        return self.run(
+            query=query,
+            documents=documents,
+            invocation_context=invocation_context,
+        )

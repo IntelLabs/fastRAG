1a6ab10504433988fdf0b63ac99f6a11bf262980
Daniel Fleischer <daniel.fleischer@intel.com>
2024-11-24 16:20:19 +0200

    Bump

--- fastrag/__init__.py
+++ fastrag/__init__.py
@@ -1,4 +1,4 @@
 ### Essential imports for loading components and modifications to original Haystack components
 from fastrag import generators, rankers, retrievers, stores
 
-__version__ = "3.1.0"
+__version__ = "3.1.1"
